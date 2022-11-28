523b604ff6706a5ab759d313bd6a4776ee281bf6
Morning <morning@mstars-lab2-03s-iMac.local>
2022-11-28 12:26:20 +0800

    day10

---
+++ day10/js/java.js
@@ -0,0 +1,46 @@
+console.log('Helloo world grom java!!!')
+console.log('hello')
+//alert('hii yu nm')
+
+var hello ;
+
+console.log(hello);
+
+
+var myNumber = 6;
+
+hello = "Helloo world grom java!!!" ;
+console.log(hello)
+
+
+hello = "alimshig yumnuud bgd bn"
+console.log(hello)
+console.log(myNumber)
+
+
+
+//console.log(PI);
+
+var huvin = 5;
+var a = 6;
+
+console.log(a);
+
+let b = 6;
+let c = 8;
+
+console.log(b);
+
+let message = 'hi';
+console.log(message);
+
+ console.log(message + b + c);
+ alert(message + b + c);
+
+ message = 8;
+ console.log(message)
+ alert(message);
+ 
+ 
+
+
